ff00f014b39304bdf0e8fef1678f1f21bdc8e214
Writt3r <joao.philipe.alcantara@gmail.com>
2026-04-27 19:26:42 -0300

    Add files via upload

--- Sistema.java
+++ Sistema.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Sistema {
+    public static boolean existeNoSistema(String cpf, GerenciadorAtivo g, ListaEspera f) {
+        return g.getAtivos().contains(cpf) || f.getFila().contains(cpf);
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         GerenciadorAtivo gerenciador = new GerenciadorAtivo();
